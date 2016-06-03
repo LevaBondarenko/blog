3183884333ad70a92c0e6dce6fe9f498eca2d6cd
LevaBond <lion.bond93@gmail.com>
2016-06-03 11:09:45 +0600

    summary

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,5 +1,20 @@
-<h1>Создание</h1>
-{!! Form::open(array('url'=>'create')) !!}
+@extends('app')
+@section('content')
+    <h1>Создание</h1>
+    {!! Form::open(array('url'=>'create')) !!}
+    <div class="form-group">
+        {!! Form::label('slug') !!}
+        {!! Form::text('title',null,['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('preview_text') !!}
+        {!! Form::text('preview_text',null,['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('detail_text') !!}
+        {!! Form::text('detail_text',null,['class'=>'form-control']) !!}
+    </div>
 
+    {!!Form::close() !!}
 
-{{Form::close()}}
+@endsection
